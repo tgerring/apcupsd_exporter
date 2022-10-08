a9afcd8baafdc99f206cab29aa095e15f95faadd
Taylor Gerring <taylor.gerring@gmail.com>
2022-10-08 11:06:28 -0400

    Enable multiple addresses to be monitored

--- cmd/apcupsd_exporter/main.go
+++ cmd/apcupsd_exporter/main.go
@@ -6,6 +6,7 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/mdlayher/apcupsd"
 	apcupsdexporter "github.com/mdlayher/apcupsd_exporter"
@@ -13,11 +14,13 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
+const DELIMIT = ","
+
 var (
 	telemetryAddr = flag.String("telemetry.addr", ":9162", "address for apcupsd exporter")
 	metricsPath   = flag.String("telemetry.path", "/metrics", "URL path for surfacing collected metrics")
 
-	apcupsdAddr    = flag.String("apcupsd.addr", ":3551", "address of apcupsd Network Information Server (NIS)")
+	apcupsdAddr    = flag.String("apcupsd.addr", ":3551", "address of apcupsd Network Information Server (NIS): separate multiple entries by comma")
 	apcupsdNetwork = flag.String("apcupsd.network", "tcp", `network of apcupsd Network Information Server (NIS): typically "tcp", "tcp4", or "tcp6"`)
 )
 
@@ -28,17 +31,22 @@ func main() {
 		log.Fatal("address of apcupsd Network Information Server (NIS) must be specified with '-apcupsd.addr' flag")
 	}
 
-	fn := newClient(*apcupsdNetwork, *apcupsdAddr)
+	// split & loop through list of apcupsd addressess
+	var addrList = strings.Split(*apcupsdAddr, DELIMIT)
+	for i := 0; i < len(addrList); i++ {
+		log.Printf("starting apcupsd exporter for server %s://%s", *apcupsdNetwork, addrList[i])
+		fn := newClient(*apcupsdNetwork, addrList[i])
+		prometheus.MustRegister(apcupsdexporter.New(fn))
+	}
 
-	prometheus.MustRegister(apcupsdexporter.New(fn))
+	// start exporter
+	log.Printf("starting prometheus exporter on %q",
+		*telemetryAddr)
 
 	http.Handle(*metricsPath, promhttp.Handler())
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, *metricsPath, http.StatusMovedPermanently)
 	})
-
-	log.Printf("starting apcupsd exporter on %q for server %s://%s",
-		*telemetryAddr, *apcupsdNetwork, *apcupsdAddr)
 
 	if err := http.ListenAndServe(*telemetryAddr, nil); err != nil {
 		log.Fatalf("cannot start apcupsd exporter: %s", err)
